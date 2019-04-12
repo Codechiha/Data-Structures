2cf3c50e7a666372e93d2662812835a2ab9f66ff
David Situ <davidsitu626@gmail.com>
2019-04-11 18:58:46 -0700

    Heap insert, delete, get_max, get_size, Bubble up Complete

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -3,19 +3,32 @@ class Heap:
     self.storage = []
 
   def insert(self, value):
-    pass
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    pass
+    deleted = self.storage[0]
+    del self.storage[0]
+    self._sift_down(0)
+    return deleted
 
   def get_max(self):
-    pass
+    return self.storage[0]
 
   def get_size(self):
-    pass
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    pass
+    #Parent Node = (Index - 1) // 2
+    #While Parent Node is 0 or greater
+    while (index - 1) // 2 >= 0:
+      parent = (index - 1) // 2
+      # if Current (Index) Node value > Parent Node value
+      if self.storage[index] > self.storage[parent]:
+          # Swap 
+        self.storage[index], self.storage[parent] = self.storage[parent], self.storage[index]
+      # Update the Indices of the swapped values 
+      index = parent
 
   def _sift_down(self, index):
     pass
